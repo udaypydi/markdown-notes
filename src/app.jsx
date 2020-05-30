eacdc824c2f176b2a2dff3ca7660eca43be52605
udaypydi <udaypydi333@gmail.com>
2020-05-30 23:51:28 +0530

    evernote auth button added

--- src/app.jsx
+++ src/app.jsx
@@ -15,12 +15,15 @@ export default function App() {
             src="https://glow-guides.disciplemedia.com/assets/google_login_button-6474359c9c06fef79ddc3d744342cca87e0b3ef8be1d0d7b8d71c22347c84075.png" 
             alt="Google" 
         />
-        <img 
-          className="m-4 bg-green-500"
-          width="100"
-          src="https://upload.wikimedia.org/wikipedia/commons/a/a4/Evernote_Icon.png" 
-          alt="Evernote" 
-        />
+        <div className="m-4 bg-green-500 flex items-center" style={{ width: 250 }}> 
+          <img 
+            width="60"
+            src="https://cdn4.iconfinder.com/data/icons/social-media-2210/24/Evernote-512.png" 
+            alt="Evernote" 
+          />
+          <p className="text-white text-2xl">Evernote</p>
+        </div>
+        
         <Button buttonText="Get Started"/>
     </div>
   );
