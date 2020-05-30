a2414c38085b1bf3aab17aa91ef511436631b34f
udaypydi <udaypydi333@gmail.com>
2020-05-30 23:52:37 +0530

    cursor pointer

--- src/app.jsx
+++ src/app.jsx
@@ -10,12 +10,12 @@ export default function App() {
     <div className="h-screen w-screen bg-gray-800 flex flex-1 flex-col justify-center items-center">
         <p className="text-5xl text-gray-200">Welcome to Notes.md!</p>
         <img 
-            className="m-4"
+            className="m-4 cursor-pointer"
             width="250"
             src="https://glow-guides.disciplemedia.com/assets/google_login_button-6474359c9c06fef79ddc3d744342cca87e0b3ef8be1d0d7b8d71c22347c84075.png" 
             alt="Google" 
         />
-        <div className="m-4 bg-green-500 flex items-center" style={{ width: 250 }}> 
+        <div className="m-4 bg-green-500 flex items-center cursor-pointer" style={{ width: 250 }}> 
           <img 
             width="60"
             src="https://cdn4.iconfinder.com/data/icons/social-media-2210/24/Evernote-512.png" 
