cdccaca911ea1300bc1a858b7541020580ef6634
udaypydi <udaypydi333@gmail.com>
2020-05-30 23:48:09 +0530

    add google auth

--- src/app.jsx
+++ src/app.jsx
@@ -9,6 +9,18 @@ export default function App() {
   return (
     <div className="h-screen w-screen bg-gray-800 flex flex-1 flex-col justify-center items-center">
         <p className="text-5xl text-gray-200">Welcome to Notes.md!</p>
+        <img 
+            className="m-4"
+            width="250"
+            src="https://glow-guides.disciplemedia.com/assets/google_login_button-6474359c9c06fef79ddc3d744342cca87e0b3ef8be1d0d7b8d71c22347c84075.png" 
+            alt="Google" 
+        />
+        <img 
+          className="m-4 bg-green-500"
+          width="100"
+          src="https://upload.wikimedia.org/wikipedia/commons/a/a4/Evernote_Icon.png" 
+          alt="Evernote" 
+        />
         <Button buttonText="Get Started"/>
     </div>
   );
