aa28f563b8c8351efcc2ea8ac4264ccceaa945ad
udaypydi <udaypydi333@gmail.com>
2020-05-30 23:38:34 +0530

    add landing page

--- src/app.jsx
+++ src/app.jsx
@@ -7,8 +7,9 @@ import './app.scss';
 
 export default function App() {
   return (
-    <div>
-      <SideBar />
+    <div className="h-screen w-screen bg-gray-800 flex flex-1 flex-col justify-center items-center">
+        <p className="text-5xl text-gray-200">Welcome to Notes.md!</p>
+        <Button buttonText="Get Started"/>
     </div>
   );
 }
