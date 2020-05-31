8f8e73801943ba362e238fe5dcc5718cd8ebe5f2
udaypydi <udaypydi333@gmail.com>
2020-05-31 23:08:02 +0530

    markdown editor added

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Button from 'uielements/button/button.component';
-import SideBar from 'components/sidebar/sidebar.component';
-import logo from './logo.png';
+import { MarkdownEditor } from 'react-markdown-editor';
 import 'assets/css/main.css';
 import './app.scss';
 
@@ -9,21 +8,7 @@ export default function App() {
   return (
     <div className="h-screen w-screen bg-gray-800 flex flex-1 flex-col justify-center items-center">
         <p className="text-5xl text-gray-200">Welcome to Notes.md!</p>
-        <img 
-            className="m-4 cursor-pointer"
-            width="250"
-            src="https://glow-guides.disciplemedia.com/assets/google_login_button-6474359c9c06fef79ddc3d744342cca87e0b3ef8be1d0d7b8d71c22347c84075.png" 
-            alt="Google" 
-        />
-        <div className="m-4 bg-green-500 flex items-center cursor-pointer" style={{ width: 250 }}> 
-          <img 
-            width="50"
-            src="https://cdn4.iconfinder.com/data/icons/social-media-2210/24/Evernote-512.png" 
-            alt="Evernote" 
-          />
-          <p className="text-white text-2xl m-2">Evernote</p>
-        </div>
-        
+        <MarkdownEditor initialContent="Test" iconsSet="font-awesome"/>
         <Button buttonText="Get Started"/>
     </div>
   );
