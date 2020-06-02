4c206f04422d39709295d9b7abbd476e9379eece
udaypydi <udaypydi333@gmail.com>
2020-06-02 23:12:13 +0530

    notes app

--- src/app.jsx
+++ src/app.jsx
@@ -8,14 +8,14 @@ export default function App() {
   const [value, setValue] = useState('');
   return (
     <div className="h-screen w-screen bg-gray-800 flex flex-1 flex-col justify-center items-center">
-        <p className="text-5xl text-gray-200">Welcome to Notes.md!</p>
+        <p className="text-5xl text-gray-200">Welcome!</p>
         <Editor 
           value={value}
           onChange={val => setValue(val)}
           placeholder="Add Notes"
           language="en"
         />
-        <Button buttonText="Get Started"/>
+        <Button className="mt-2" buttonText="Get Started"/>
     </div>
   );
 }
