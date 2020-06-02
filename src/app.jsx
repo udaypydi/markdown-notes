c178c8c3ccc454aa5df9d7fa268ccb6c35b4a96e
udaypydi <udaypydi333@gmail.com>
2020-06-02 23:12:53 +0530

    revert notes md

--- src/app.jsx
+++ src/app.jsx
@@ -8,7 +8,7 @@ export default function App() {
   const [value, setValue] = useState('');
   return (
     <div className="h-screen w-screen bg-gray-800 flex flex-1 flex-col justify-center items-center">
-        <p className="text-5xl text-gray-200">Welcome!</p>
+        <p className="text-5xl text-gray-200">Welcome To Notes.md!</p>
         <Editor 
           value={value}
           onChange={val => setValue(val)}
