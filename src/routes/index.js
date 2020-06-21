03ad2b8330c749a662ac5523694dc43721413d1d
udaypydi <udaypydi333@gmail.com>
2020-06-21 15:28:38 +0530

    feat(default router): default config based router added

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,36 @@ import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import App from '../app';
 import NewNotes from 'components/newnotes/newnotes.component';
 
+function DefaultLayout({config, component }) {
+    return (
+        <Route
+            exact
+            path={config.path}
+            render={(props) => (
+                <>
+                {
+                    config.header && (
+                        <h1>Header</h1>
+                    )
+                }
+                {React.createElement(component, [props])}
+                </>
+            )}
+        />
+    )
+}
 function AppRouter() {
     return (
         <Router>
             <Switch>
                 <Route exact path="/" component={App} />
-                <Route exact patch="/new-notes" component={NewNotes} />
+                <DefaultLayout 
+                    config={{ 
+                        path: '/new-notes', 
+                        header: true 
+                    }} 
+                    component={NewNotes} 
+                />
             </Switch>
         </Router>
     )
